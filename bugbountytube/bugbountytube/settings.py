b2a251879bc560e57edfe3eaf76a75120475e5d0
Patrik <PatrikFehrenbach@users.noreply.github.com>
2023-10-06 23:35:17 +0200

    test

--- bugbountytube/bugbountytube/settings.py
+++ bugbountytube/bugbountytube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bym030g!9%ywrl^qxzhcfh=6o448g^(_+gq=m(m%8l^k67jh@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
