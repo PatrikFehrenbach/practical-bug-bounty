5bd4326ec38a5f963350bfc43b79f94c442e6de1
Dwi Siswanto <me@dw1.io>
2023-10-07 18:37:21 +0700

    feat: add URL pattern for static files (fix #2)

--- bugbountytube/bugbountytube/urls.py
+++ bugbountytube/bugbountytube/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,5 +23,7 @@ urlpatterns = [
     path('', include('videos.urls')),
     path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('markdownx/', include('markdownx.urls')),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
