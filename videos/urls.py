288fc30da41cb9f7310db2d309106e065d944c77
Patrik <PatrikFehrenbach@users.noreply.github.com>
2023-10-08 22:02:24 +0200

    new changes

--- videos/urls.py
+++ videos/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contribute/', views.contribute, name='contribute'),
     path('philosophy/', views.philosophy, name='philosophy'),
+    path('contact/', views.contact, name='contact'),
 #    path('category/<int:category_id>/', views.category_videos, name='category_videos'),
 
 ]
