2e9ffc82c760c53467cc5864103afc35b6b2ff33
Patrik <PatrikFehrenbach@users.noreply.github.com>
2023-10-08 11:00:39 +0200

    New Database Layout changes

--- bugbountytube/videos/urls.py
+++ bugbountytube/videos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('category/<int:category_id>/', views.category_videos, name='category_videos'),
+#    path('category/<int:category_id>/', views.category_videos, name='category_videos'),
 
 ]
