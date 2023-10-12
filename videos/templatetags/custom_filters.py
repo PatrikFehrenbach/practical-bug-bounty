bcc7efbba102effc6c8a176f52227aa23422fb9d
Patrik <PatrikFehrenbach@users.noreply.github.com>
2023-10-12 14:24:53 +0200

    added

--- videos/templatetags/custom_filters.py
+++ videos/templatetags/custom_filters.py
@@ -2,6 +2,6 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='get_item')
-def get_item(dictionary, key):
-    return dictionary.get(key)
+@register.filter(name='get_icon')
+def get_icon(icons_dict, key):
+    return icons_dict.get(key, '')
