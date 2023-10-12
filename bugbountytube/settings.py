5e492ceace5627d277bddc4bf8c1230547d72241
Patrik <PatrikFehrenbach@users.noreply.github.com>
2023-10-12 13:54:28 +0200

    sentry support

--- bugbountytube/settings.py
+++ bugbountytube/settings.py
@@ -13,9 +13,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import sentry_sdk
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_DSN'),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 
 # Quick-start development settings - unsuitable for production
